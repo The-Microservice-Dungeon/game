6038b29051ee31fb6e94b56bc3dbab2a37c018b7
dadepu <danielkoellgen@gmail.com>
2021-11-03 15:18:05 +0100

    added RoundService

--- src/main/kotlin/microservice/dungeon/game/aggregates/round/services/RoundService.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/round/services/RoundService.kt
@@ -1,5 +1,6 @@
 package microservice.dungeon.game.aggregates.round.services
 
+import microservice.dungeon.game.aggregates.core.EntityAlreadyExistsException
 import microservice.dungeon.game.aggregates.round.domain.Round
 import microservice.dungeon.game.aggregates.round.repositories.RoundRepository
 import org.springframework.beans.factory.annotation.Autowired
@@ -10,48 +11,59 @@ class RoundService @Autowired constructor (
     private val roundRepository: RoundRepository
 ) {
     fun startNewRound(roundNumber: Int) {
-        // TODO(check if round already exists and throw)
+        if (!roundRepository.findByRoundNumber(roundNumber).isEmpty) {
+            throw EntityAlreadyExistsException("Round $roundNumber already exists.")
+        }
         val round = Round(roundNumber)
         roundRepository.save(round)
     }
 
     fun endCommandInputs(roundNumber: Int) {
-
+        val round = roundRepository.findByRoundNumber(roundNumber).get()
+        round.endCommandInputPhase()
+        roundRepository.save(round)
     }
 
     fun deliverBlockingCommands(roundNumber: Int) {
-
+        val round = roundRepository.findByRoundNumber(roundNumber).get()
+        // TODO
+        roundRepository.save(round)
     }
 
     fun deliverTradingCommands(roundNumber: Int) {
-
+        val round = roundRepository.findByRoundNumber(roundNumber).get()
+        // TODO
+        roundRepository.save(round)
     }
 
     fun deliverMovementCommands(roundNumber: Int) {
-
+        val round = roundRepository.findByRoundNumber(roundNumber).get()
+        // TODO
+        roundRepository.save(round)
     }
 
     fun deliverBattleCommands(roundNumber: Int) {
-
+        val round = roundRepository.findByRoundNumber(roundNumber).get()
+        // TODO
+        roundRepository.save(round)
     }
 
     fun deliverMiningCommands(roundNumber: Int) {
-
+        val round = roundRepository.findByRoundNumber(roundNumber).get()
+        // TODO
+        roundRepository.save(round)
     }
 
     fun deliverScoutingCommands(roundNumber: Int) {
-
+        val round = roundRepository.findByRoundNumber(roundNumber).get()
+        // TODO
+        roundRepository.save(round)
     }
 
     fun endRound(roundNumber: Int) {
-        val round = roundRepository.findByRoundNumber(roundNumber)
+        val round = roundRepository.findByRoundNumber(roundNumber).get()
         round.endRound()
         roundRepository.save(round)
     }
 }
 
-// TODO(PERSISTENCE)
-// TODO(COMMAND SERVICE)
-// TODO(REST CLIENT)
-// TODO(EVENT PUBLISHER)
-// TODO(LOCALDATETIME OR DATE OR TIME?)
