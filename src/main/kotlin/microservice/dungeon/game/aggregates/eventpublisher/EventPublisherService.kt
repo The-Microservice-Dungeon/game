ab92cfd26d1d8cafa20e3a34292da99eb2c3802e
dadepu <danielkoellgen@gmail.com>
2021-11-04 21:33:36 +0100

    updated EventPublisherService, replaced type EventPublishing with Event

--- src/main/kotlin/microservice/dungeon/game/aggregates/eventpublisher/EventPublisherService.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/eventpublisher/EventPublisherService.kt
@@ -1,6 +1,6 @@
 package microservice.dungeon.game.aggregates.eventpublisher
 
-import microservice.dungeon.game.aggregates.core.EventPublishing
+import microservice.dungeon.game.aggregates.core.Event
 import microservice.dungeon.game.aggregates.eventstore.services.EventStoreService
 import microservice.dungeon.game.messaging.KafkaProducer
 import org.springframework.beans.factory.annotation.Autowired
@@ -9,9 +9,9 @@ class EventPublisherService @Autowired constructor(
     private val kafkaProducer: KafkaProducer,
     private val eventStoreService: EventStoreService
 ) {
-    fun publishEvents(events: List<EventPublishing>) {
-        events.forEach { event: EventPublishing ->
-            kafkaProducer.send(event.getTopic(), event.serializeEvent())
+    fun publishEvents(events: List<Event>) {
+        events.forEach { event: Event ->
+            kafkaProducer.send(event.getTopic(), event.serialized())
         }
         eventStoreService.markAsPublished(events.map{ event -> event.getId()})
     }
