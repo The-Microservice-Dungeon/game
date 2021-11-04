3c75631e01c3f04627cd0f3d207e321324b9e3cf
dadepu <danielkoellgen@gmail.com>
2021-11-04 21:38:39 +0100

    updated EventStoreService, conformed to type Event and EventDescriptors removed constructor

--- src/main/kotlin/microservice/dungeon/game/aggregates/eventstore/services/EventStoreService.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/eventstore/services/EventStoreService.kt
@@ -1,37 +1,43 @@
 package microservice.dungeon.game.aggregates.eventstore.services
 
+import microservice.dungeon.game.aggregates.core.Event
 import microservice.dungeon.game.aggregates.core.InvalidApplicationPropertyException
-import microservice.dungeon.game.aggregates.core.EventPublishing
 import microservice.dungeon.game.aggregates.eventstore.domain.EventDescriptor
 import microservice.dungeon.game.aggregates.eventstore.domain.EventDescriptorStatus
 import microservice.dungeon.game.aggregates.eventstore.repositories.EventDescriptorRepository
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.context.ApplicationContext
+import org.springframework.core.env.Environment
+import org.springframework.stereotype.Service
 import java.util.*
+import javax.transaction.Transactional
 
-class EventStoreService @Autowired constructor(
-    private val eventdescriptorRepository: EventDescriptorRepository,
+@Service
+open class EventStoreService @Autowired constructor(
+    private val eventDescriptorRepository: EventDescriptorRepository,
     private val applicationContext: ApplicationContext,
+    private val environment: Environment,
     @Value(value = "\${eventStore.publishingMode}")
     private val publishingMode: String
 ) {
 
-    fun storeEvent(event: EventPublishing) {
-        val eventDescriptor = EventDescriptor(event, applicationContext)
-        eventdescriptorRepository.save(eventDescriptor)
+    fun storeEvent(event: Event) {
+        val eventDescriptor = EventDescriptor(event)
+        eventDescriptorRepository.save(eventDescriptor)
     }
 
-    fun markAsPublished(events: List<UUID>) {
+    @Transactional
+    open fun markAsPublished(events: List<UUID>) {
         when(publishingMode) {
-            "UPDATE" -> eventdescriptorRepository.markAsPublished(events)
-            "DELETE" -> eventdescriptorRepository.deletePublished(events)
+            "UPDATE" -> eventDescriptorRepository.markAsPublished(events)
+            "DELETE" -> eventDescriptorRepository.deletePublished(events)
             else -> throw InvalidApplicationPropertyException("eventStore.publishingMode must either be UPDATE or DELETE")
         }
     }
 
-    fun fetchUnpublishedEvents(): List<EventPublishing> {
-        return eventdescriptorRepository.findByStatus(EventDescriptorStatus.CREATED)
-            .map{ descriptor: EventDescriptor -> descriptor.getAsEventPublishing()}
+    fun fetchUnpublishedEvents(): List<Event> {
+        return eventDescriptorRepository.findByStatus(EventDescriptorStatus.CREATED)
+            .map{ descriptor: EventDescriptor -> descriptor.getAsEvent(environment, applicationContext)}
     }
 }
