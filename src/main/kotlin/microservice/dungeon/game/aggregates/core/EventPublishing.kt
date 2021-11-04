8d81c33fb04b0c2c4ab04cf811b728606ecfd15b
dadepu <danielkoellgen@gmail.com>
2021-11-04 21:25:47 +0100

    removed EventPublishing, all events, outgoing and ingoing must now conform to typ Event

--- src/main/kotlin/microservice/dungeon/game/aggregates/core/EventPublishing.kt
+++
@@ -1,9 +0,0 @@
-package microservice.dungeon.game.aggregates.core
-
-interface EventPublishing: Event {
-    fun serializeEvent(): String
-
-    fun initializeFromSerializedEvent(event: String)
-
-    fun getTopic(): String
-}
