bc7cead445c4dbb00c3cb7e38bc6cbe61bad3d32
dadepu <danielkoellgen@gmail.com>
2021-11-08 07:02:47 +0100

    refactor(web/commandDispatcher): removed example client and test

--- src/main/kotlin/microservice/dungeon/game/web/CommandDispatcherClient.kt
+++ src/main/kotlin/microservice/dungeon/game/web/CommandDispatcherClient.kt
@@ -1,24 +1,29 @@
 package microservice.dungeon.game.web
 
 import com.fasterxml.jackson.databind.JsonNode
+import com.fasterxml.jackson.databind.ObjectMapper
+import org.springframework.http.HttpHeaders
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
 import org.springframework.web.reactive.function.client.WebClient
+import org.springframework.web.reactive.function.client.awaitExchange
+import org.springframework.web.reactive.function.client.bodyToMono
+import org.springframework.web.reactive.function.client.createExceptionAndAwait
 
 class CommandDispatcherClient constructor(
     private val robotBaseUrl: String
 ) {
     private val robotWebClient = WebClient.create(robotBaseUrl)
 
-    fun getUserById(): JsonNode? {
+    fun dispatchBlockingCommands(roundNumber: Int, commands: List<String>): JsonNode? {
         return robotWebClient
-            .get()
-            .uri("/users/{id}", 1)
+            .post()
+            .uri("/commands/blocking")
+            .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+            .bodyValue(ObjectMapper().writeValueAsString(commands))
             .retrieve()
             .bodyToMono(JsonNode::class.java)
             .block()
-    }
-
-    fun dispatchBlockingCommands(roundNumber: Int, commands: List<String>) {
-        //TODO
     }
 
     fun dispatchTradingCommands(roundNumber: Int, commands: List<String>) {
