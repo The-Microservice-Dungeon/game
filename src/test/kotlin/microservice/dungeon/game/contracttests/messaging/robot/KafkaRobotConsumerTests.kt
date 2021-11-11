737bde5faf400bc84a3edb8ff9cc492802e72b67
dadepu <danielkoellgen@gmail.com>
2021-11-11 20:05:19 +0100

    refactor(tests/kafkaRobotConsumer): add structure and unfinished test cases for robotConsumer and corresponding events

---
+++ src/test/kotlin/microservice/dungeon/game/contracttests/messaging/robot/KafkaRobotConsumerTests.kt
@@ -0,0 +1,16 @@
+package microservice.dungeon.game.contracttests.messaging.robot
+
+import org.junit.jupiter.api.Test
+
+class KafkaRobotConsumerTests {
+
+    @Test
+    fun buildRobotCreatedEventTest() {
+        //TODO
+    }
+
+    @Test
+    fun buildRobotDestroyedEventTest() {
+        //TODO
+    }
+}
