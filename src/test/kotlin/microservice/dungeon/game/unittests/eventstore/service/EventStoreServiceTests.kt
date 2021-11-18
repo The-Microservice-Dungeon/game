58b3c6f00705068438cc8e628f1f4ad31967f774
dadepu <danielkoellgen@gmail.com>
2021-11-18 20:20:23 +0100

    test(unit/eventStore): extend test coverage and refactor tests

--- src/test/kotlin/microservice/dungeon/game/unittests/eventstore/service/EventStoreServiceTests.kt
+++ src/test/kotlin/microservice/dungeon/game/unittests/eventstore/service/EventStoreServiceTests.kt
@@ -1,20 +1,20 @@
 package microservice.dungeon.game.unittests.eventstore.service
 
 import microservice.dungeon.game.aggregates.core.Event
-import microservice.dungeon.game.aggregates.core.InvalidApplicationPropertyException
+import microservice.dungeon.game.aggregates.eventstore.domain.EventDescriptor
 import microservice.dungeon.game.aggregates.eventstore.domain.EventDescriptorStatus
 import microservice.dungeon.game.aggregates.eventstore.repositories.EventDescriptorRepository
 import microservice.dungeon.game.aggregates.eventstore.services.EventStoreService
 import microservice.dungeon.game.aggregates.round.domain.RoundStatus
 import microservice.dungeon.game.aggregates.round.events.RoundStarted
-import org.junit.jupiter.api.Assertions.assertThrows
+import microservice.dungeon.game.aggregates.round.events.RoundStartedBuilder
+import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
-import org.mockito.kotlin.argThat
-import org.mockito.kotlin.verify
-import org.mockito.kotlin.whenever
-import org.springframework.beans.factory.annotation.Value
+import org.mockito.kotlin.*
 import org.springframework.context.ApplicationContext
 import org.springframework.core.env.Environment
 import java.time.LocalDateTime
@@ -27,56 +27,102 @@ class EventStoreServiceTests {
     private val publishingMode: String = "UPDATING"
     private var eventStoreService: EventStoreService? = null
 
+    private var someValidEvent: Event? = null
+
+
     @BeforeEach
     fun setUp() {
         eventDescriptorRepositoryMock = mock(EventDescriptorRepository::class.java)
         applicationContextMock = mock(ApplicationContext::class.java)
         environmentMock = mock(Environment::class.java)
         eventStoreService = EventStoreService(eventDescriptorRepositoryMock!!, applicationContextMock!!, environmentMock!!, publishingMode)
+
+        someValidEvent = RoundStarted(UUID.randomUUID(), LocalDateTime.now(), UUID.randomUUID(), UUID.randomUUID(), 3, RoundStatus.COMMAND_INPUT_STARTED)
     }
 
-    @Test
-    fun storeEventTest() {
-        val event = RoundStarted(UUID.randomUUID(), LocalDateTime.now(), UUID.randomUUID(), UUID.randomUUID(), 3, RoundStatus.COMMAND_INPUT_STARTED)
-        eventStoreService!!.storeEvent(event)
-
-        verify(eventDescriptorRepositoryMock!!).save(argThat { eventDescriptor ->
-            eventDescriptor.getId() == event.getId() &&
-            eventDescriptor.getStatus() == EventDescriptorStatus.CREATED
-        })
-    }
 
     @Test
-    fun markAsPublishedWithUpdateTest() {
-        val ids = listOf(UUID.randomUUID())
-        eventStoreService = EventStoreService(eventDescriptorRepositoryMock!!, applicationContextMock!!, environmentMock!!, "UPDATE")
+    fun storeEventShouldSaveEventDescriptor() {
+        val eventDescriptor = EventDescriptor(someValidEvent!!)
+        whenever(eventDescriptorRepositoryMock!!.findById(any()))
+            .thenReturn(Optional.empty())
 
-        eventStoreService!!.markAsPublished(ids)
-        verify(eventDescriptorRepositoryMock!!).markAsPublished(ids)
-    }
+        eventStoreService!!.storeEvent(someValidEvent!!)
 
-    @Test
-    fun markAsPublishedWithDeleteTest() {
-        val ids = listOf(UUID.randomUUID())
-        eventStoreService = EventStoreService(eventDescriptorRepositoryMock!!, applicationContextMock!!, environmentMock!!, "DELETE")
-
-        eventStoreService!!.markAsPublished(ids)
-        verify(eventDescriptorRepositoryMock!!).deletePublished(ids)
-    }
-
-    @Test
-    fun markAsPublishedInvalidConfigTest() {
-        val ids = listOf(UUID.randomUUID())
-        eventStoreService = EventStoreService(eventDescriptorRepositoryMock!!, applicationContextMock!!, environmentMock!!, "")
-
-        assertThrows(InvalidApplicationPropertyException::class.java) {
-            eventStoreService!!.markAsPublished(ids)
+        argumentCaptor<EventDescriptor>().apply {
+            verify(eventDescriptorRepositoryMock!!).save(capture())
+            assertTrue(firstValue.isSameAs(eventDescriptor))
         }
     }
 
     @Test
-    fun fetchUnpublishedEventsTest() {
+    fun storeEventShouldThrowWhenEventAlreadyExists() {
+        val mockEventDescriptor: EventDescriptor = mock()
+        whenever(eventDescriptorRepositoryMock!!.findById(someValidEvent!!.getId()))
+            .thenReturn(Optional.of(mockEventDescriptor))
+
+        assertThatThrownBy {
+            eventStoreService!!.storeEvent(someValidEvent!!)
+        }
+    }
+
+
+    @Test
+    fun markAsPublishedShouldUpdateWhenModeIsUpdate() {
+        val listOfValidIds = listOf(UUID.randomUUID())
+        val publishMode = "UPDATE"
+        eventStoreService = EventStoreService(eventDescriptorRepositoryMock!!, applicationContextMock!!, environmentMock!!, publishMode)
+
+        eventStoreService!!.markAsPublished(listOfValidIds)
+
+        verify(eventDescriptorRepositoryMock!!).markAsPublished(listOfValidIds)
+    }
+
+    @Test
+    fun markAsPublishedShouldDeleteWhenModeIsDelete() {
+        val listOfValidIds = listOf(UUID.randomUUID())
+        val publishMode = "DELETE"
+        eventStoreService = EventStoreService(eventDescriptorRepositoryMock!!, applicationContextMock!!, environmentMock!!, publishMode)
+
+        eventStoreService!!.markAsPublished(listOfValidIds)
+
+        verify(eventDescriptorRepositoryMock!!).deletePublished(listOfValidIds)
+    }
+
+    @Test
+    fun markAsPublishedShouldThrowWhenModeIsInvalid() {
+        val listOfValidIds = listOf(UUID.randomUUID())
+        val invalidPublishMode = ""
+
+        eventStoreService = EventStoreService(eventDescriptorRepositoryMock!!, applicationContextMock!!, environmentMock!!, invalidPublishMode)
+
+        assertThatThrownBy {
+            eventStoreService!!.markAsPublished(listOfValidIds)
+        }
+    }
+
+
+    @Test
+    fun fetchUnpublishedEventsShouldLoadEventDescriptors() {
         eventStoreService!!.fetchUnpublishedEvents()
         verify(eventDescriptorRepositoryMock!!).findByStatus(EventDescriptorStatus.CREATED)
     }
+
+    @Test
+    fun fetchUnpublishedEventsShouldMapToEventsWhenLoaded() {
+        val validEventDescriptor = EventDescriptor(someValidEvent!!)
+        whenever(eventDescriptorRepositoryMock!!.findByStatus(EventDescriptorStatus.CREATED))
+            .thenReturn(listOf(validEventDescriptor))
+        whenever(environmentMock!!.getProperty("eventStore.builderSuffix")).thenReturn("Builder")
+        whenever(applicationContextMock!!.getBean("roundStartedBuilder")).thenReturn(RoundStartedBuilder())
+
+        val listOfEvents = eventStoreService!!.fetchUnpublishedEvents()
+
+        assertThat(listOfEvents)
+            .hasSize(1)
+            .anyMatch { event ->
+                event.isSameAs(someValidEvent!!)
+            }
+    }
+
 }
