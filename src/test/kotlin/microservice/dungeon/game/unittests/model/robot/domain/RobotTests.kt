eea66e1b7347457b363e34f9cd8916a07f99a3b4
dadepu <danielkoellgen@gmail.com>
2021-11-19 10:06:46 +0100

    test(robot): add robotTests for initialization and destruction

--- src/test/kotlin/microservice/dungeon/game/unittests/model/robot/domain/RobotTests.kt
+++ src/test/kotlin/microservice/dungeon/game/unittests/model/robot/domain/RobotTests.kt
@@ -1,4 +1,31 @@
 package microservice.dungeon.game.unittests.model.robot.domain
 
+import microservice.dungeon.game.aggregates.robot.domain.Robot
+import microservice.dungeon.game.aggregates.robot.domain.RobotStatus
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.util.*
+
 class RobotTests {
+    private var validRobot: Robot? = null
+
+    @BeforeEach
+    fun setUp() {
+        validRobot = Robot(UUID.randomUUID(), UUID.randomUUID(), UUID.randomUUID())
+    }
+
+    @Test
+    fun newRobotShouldBeInitialized() {
+        assertThat(validRobot!!.getRobotStatus())
+            .isEqualTo(RobotStatus.ACTIVE)
+    }
+
+    @Test
+    fun destroyRobotShouldMakeRobotInactive() {
+        validRobot!!.destroyRobot()
+
+        assertThat(validRobot!!.getRobotStatus())
+            .isEqualTo(RobotStatus.INACTIVE)
+    }
 }
