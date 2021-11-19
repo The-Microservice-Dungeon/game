2cf78e47b6faa994e403de57ba41aef290ad3d31
dadepu <danielkoellgen@gmail.com>
2021-11-19 16:25:55 +0100

    feat(kafkaProducer): implement send method to conform with expected event structure

--- src/main/kotlin/microservice/dungeon/game/messaging/producer/KafkaProducer.kt
+++ src/main/kotlin/microservice/dungeon/game/messaging/producer/KafkaProducer.kt
@@ -1,8 +1,12 @@
 package microservice.dungeon.game.messaging.producer
 
+import microservice.dungeon.game.aggregates.core.Event
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.RecordHeader
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.kafka.core.KafkaTemplate
 import org.springframework.stereotype.Component
+import java.time.format.DateTimeFormatter
 
 @Component
 class KafkaProducer @Autowired constructor (
@@ -11,5 +15,25 @@ class KafkaProducer @Autowired constructor (
 
     override fun send(topic: String, payload: String) {
         kafkaTemplate.send(topic, payload)
+    }
+
+    fun send(event: Event) {
+        val record = ProducerRecord<String, String>(event.getTopic(), event.toDTO().serialize())
+        record.headers().add(
+            RecordHeader("eventId", event.getId().toString().toByteArray())
+        )
+        record.headers().add(
+            RecordHeader("transactionId", event.getTransactionId().toString().toByteArray())
+        )
+        record.headers().add(
+            RecordHeader("version", event.getVersion().toString().toByteArray())
+        )
+        record.headers().add(
+            RecordHeader("timestamp", event.getOccurredAt().getFormattedTime().toByteArray())
+        )
+        record.headers().add(
+            RecordHeader("type", event.getEventName().toByteArray())
+        )
+        kafkaTemplate.send(record)
     }
 }
