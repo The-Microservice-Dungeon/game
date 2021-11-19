200c960d4f1c5c0e5944b13755114fbadd25b56d
dadepu <danielkoellgen@gmail.com>
2021-11-19 12:01:45 +0100

    test(kafkaProducer): add kafka-producer-listener-test to verify eventProducer is called when event is successfully published

--- src/test/kotlin/microservice/dungeon/game/unittests/messaging/producer/KafkaProducerListenerTests.kt
+++ src/test/kotlin/microservice/dungeon/game/unittests/messaging/producer/KafkaProducerListenerTests.kt
@@ -1,33 +1,43 @@
 package microservice.dungeon.game.unittests.messaging.producer
 
+import microservice.dungeon.game.aggregates.eventpublisher.EventPublisherService
 import microservice.dungeon.game.messaging.producer.KafkaProducerListener
+import org.apache.kafka.clients.producer.ProducerRecord
+import org.apache.kafka.common.header.internals.RecordHeader
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
+import org.mockito.kotlin.any
+import org.mockito.kotlin.mock
+import org.mockito.kotlin.verify
+import org.mockito.kotlin.whenever
 import org.springframework.context.ApplicationContext
+import java.util.*
 
 class KafkaProducerListenerTests {
+    private var eventPublisherServiceMock: EventPublisherService? = null
     private var applicationContextMock: ApplicationContext? = null
     private var kafkaProducerListener: KafkaProducerListener<String, String>? = null
 
+    private val KAFKA_MESSAGE_HEADER_EVENTID = "eventId"
+
     @BeforeEach
-    fun initialize() {
-        applicationContextMock = mock(ApplicationContext::class.java)
-        kafkaProducerListener = KafkaProducerListener(applicationContextMock!!)
+    fun setUp() {
+        eventPublisherServiceMock = mock()
+        applicationContextMock = mock()
+        whenever(applicationContextMock!!.getBean("eventPublisherService"))
+            .thenReturn(eventPublisherServiceMock!!)
+        kafkaProducerListener = KafkaProducerListener(applicationContextMock!!, KAFKA_MESSAGE_HEADER_EVENTID)
     }
 
     @Test
-    fun onSuccessCorrectMessageTest() {
-        //TODO
-    }
+    fun onSuccessShouldNotifyEventPublisherWithEventIdWhenProducerRecordIsValid() {
+        val validEventId = UUID.randomUUID()
+        val validProducerRecord = ProducerRecord<String, String>("someTopic", "someContent")
+        validProducerRecord.headers().add(RecordHeader(KAFKA_MESSAGE_HEADER_EVENTID, validEventId.toString().toByteArray()))
 
-    @Test
-    fun onSuccessFaultyMessageTest() {
-        //TODO
-    }
+        kafkaProducerListener!!.onSuccess(validProducerRecord, null)
 
-    @Test
-    fun onErrorTest() {
-
+        verify(eventPublisherServiceMock!!).onSuccessfulPublish(validEventId)
     }
 }
