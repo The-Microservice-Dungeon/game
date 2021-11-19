c73cc8e8dbe4a3a2bd6093b222eeb48493c360ab
dadepu <danielkoellgen@gmail.com>
2021-11-19 08:18:00 +0100

    test(eventStore): refactor eventDescriptorTests with custom assertions

--- src/test/kotlin/microservice/dungeon/game/unittests/eventstore/domain/EventDescriptorTests.kt
+++ src/test/kotlin/microservice/dungeon/game/unittests/eventstore/domain/EventDescriptorTests.kt
@@ -6,11 +6,14 @@ import microservice.dungeon.game.aggregates.eventstore.domain.EventDescriptorSta
 import microservice.dungeon.game.aggregates.round.domain.RoundStatus
 import microservice.dungeon.game.aggregates.round.events.RoundStarted
 import microservice.dungeon.game.aggregates.round.events.RoundStartedBuilder
+import org.assertj.core.api.Assertions.assertThat
+import microservice.dungeon.game.assertions.CustomAssertions.Companion.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
 import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.mockito.Mockito.mock
 import org.mockito.kotlin.whenever
-import org.springframework.beans.BeansException
 import org.springframework.context.ApplicationContext
 import org.springframework.core.env.Environment
 import java.lang.RuntimeException
@@ -18,43 +21,51 @@ import java.time.LocalDateTime
 import java.util.*
 
 class EventDescriptorTests {
+    private var environmentMock: Environment? = null
+    private var applicationContextMock: ApplicationContext? = null
+
+    private var someValidEvent: Event? = null
+
+    @BeforeEach
+    fun setUp() {
+        environmentMock = mock(Environment::class.java)
+        applicationContextMock = mock(ApplicationContext::class.java)
+
+        someValidEvent = RoundStarted(UUID.randomUUID(), LocalDateTime.now(), UUID.randomUUID(), UUID.randomUUID(), 3, RoundStatus.COMMAND_INPUT_STARTED)
+    }
+
 
     @Test
-    fun makeEventDescriptorFromEventTest() {
-        val event: Event = RoundStarted(UUID.randomUUID(), LocalDateTime.now(), UUID.randomUUID(), UUID.randomUUID(), 3, RoundStatus.COMMAND_INPUT_STARTED)
-        val eventDescriptor = EventDescriptor(event)
-        assertEquals(eventDescriptor.getId(), event.getId())
-        assertEquals(eventDescriptor.getType(), event.getEventName())
-        assertEquals(eventDescriptor.getOccurredAt(), event.getOccurredAt())
-        assertEquals(eventDescriptor.getContent(), event.serialized())
-        assertEquals(eventDescriptor.getStatus(), EventDescriptorStatus.CREATED)
+    fun newEventDescriptorFromEvent() {
+        val eventDescriptor = EventDescriptor(someValidEvent!!)
 
+        assertThat(eventDescriptor)
+            .matches(someValidEvent!!)
+        assertThat(eventDescriptor.getStatus())
+            .isEqualTo(EventDescriptorStatus.CREATED)
     }
 
     @Test
-    fun makeEventFromDescriptorTest() {
-        val event: RoundStarted = RoundStarted(UUID.randomUUID(), LocalDateTime.now(), UUID.randomUUID(), UUID.randomUUID(), 3, RoundStatus.COMMAND_INPUT_STARTED)
-        val eventDescriptor = EventDescriptor(event)
-        val environmentMock = mock(Environment::class.java)
-        val applicationContextMock = mock(ApplicationContext::class.java)
-        whenever(environmentMock.getProperty("eventStore.builderSuffix")).thenReturn("Builder")
-        whenever(applicationContextMock.getBean("roundStartedBuilder")).thenReturn(RoundStartedBuilder())
-        val deserializedEvent = eventDescriptor.getAsEvent(environmentMock, applicationContextMock) as RoundStarted
+    fun getAsEventShouldBuildEventFromDescriptor() {
+        val validEventDescriptor = EventDescriptor(someValidEvent!!)
+        whenever(environmentMock!!.getProperty("eventStore.builderSuffix")).thenReturn("Builder")
+        whenever(applicationContextMock!!.getBean("roundStartedBuilder")).thenReturn(RoundStartedBuilder())
 
-        assertTrue(deserializedEvent.equals(event))
+        val createdEvent = validEventDescriptor.getAsEvent(environmentMock!!, applicationContextMock!!)
+
+        assertThat(createdEvent)
+            .isSameAs(someValidEvent!!)
+            .matches(validEventDescriptor)
     }
 
     @Test
-    fun makeEventFromDescriptorNoBuilderFoundTest() {
-        val event: RoundStarted = RoundStarted(UUID.randomUUID(), LocalDateTime.now(), UUID.randomUUID(), UUID.randomUUID(), 3, RoundStatus.COMMAND_INPUT_STARTED)
-        val eventDescriptor = EventDescriptor(event)
-        val environmentMock = mock(Environment::class.java)
-        val applicationContextMock = mock(ApplicationContext::class.java)
-        whenever(environmentMock.getProperty("eventStore.builderSuffix")).thenReturn("Builder")
-        whenever(applicationContextMock.getBean("roundStartedBuilder")).thenThrow(RuntimeException::class.java)
+    fun getAsEventShouldThrowWhenEventBuilderDoesNotExist() {
+        val validEventDescriptor = EventDescriptor(someValidEvent!!)
+        whenever(environmentMock!!.getProperty("eventStore.builderSuffix")).thenReturn("Builder")
+        whenever(applicationContextMock!!.getBean("roundStartedBuilder")).thenThrow(RuntimeException::class.java)
 
-        assertThrows(Exception::class.java) {
-            val deserializedEvent = eventDescriptor.getAsEvent(environmentMock, applicationContextMock) as RoundStarted
+        assertThatThrownBy {
+            validEventDescriptor.getAsEvent(environmentMock!!, applicationContextMock!!)
         }
     }
 }
