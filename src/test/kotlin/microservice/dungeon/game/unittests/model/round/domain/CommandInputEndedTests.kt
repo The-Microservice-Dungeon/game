c3656969278f1746aca5b7d1477b9194cd44e027
dadepu <danielkoellgen@gmail.com>
2021-11-20 08:33:19 +0100

    test(round): implement unit event tests

--- src/test/kotlin/microservice/dungeon/game/unittests/model/round/domain/CommandInputEndedTests.kt
+++ src/test/kotlin/microservice/dungeon/game/unittests/model/round/domain/CommandInputEndedTests.kt
@@ -1,29 +1,42 @@
-//package microservice.dungeon.game.unittests.model.round.domain
-//
-//import microservice.dungeon.game.aggregates.round.domain.Round
-//import microservice.dungeon.game.aggregates.round.domain.RoundStatus
-//import microservice.dungeon.game.aggregates.round.events.CommandInputEnded
-//import org.junit.jupiter.api.Assertions
-//import org.junit.jupiter.api.Assertions.assertEquals
-//import org.junit.jupiter.api.Test
-//import java.lang.Exception
-//import java.util.*
-//
-//class CommandInputEndedTests {
-//
-//    @Test
-//    fun checkDefaultValuesTest() {
-//        val round = Round(UUID.randomUUID(), 3, UUID.randomUUID(), RoundStatus.COMMAND_INPUT_ENDED)
-//        val commandInputEnded = CommandInputEnded(round)
-//        assertEquals(commandInputEnded.getTopic(), "testTopic")
-//        assertEquals(commandInputEnded.getEventName(), "commandInputEnded")
-//    }
-//
-//    @Test
-//    fun preventCommandInputEndedCreationWithWrongRoundStatusTest() {
-//        val round = Round(UUID.randomUUID(), 3, UUID.randomUUID(), RoundStatus.COMMAND_INPUT_STARTED)
-//        Assertions.assertThrows(Exception::class.java) {
-//            val commandInputEnded = CommandInputEnded(round)
-//        }
-//    }
-//}
+package microservice.dungeon.game.unittests.model.round.domain
+
+import microservice.dungeon.game.aggregates.round.domain.Round
+import microservice.dungeon.game.aggregates.round.domain.RoundStatus
+import microservice.dungeon.game.aggregates.round.events.CommandInputEnded
+import microservice.dungeon.game.aggregates.round.events.RoundEnded
+import microservice.dungeon.game.assertions.CustomAssertions
+import microservice.dungeon.game.assertions.CustomAssertions.Companion.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import java.lang.Exception
+import java.util.*
+
+class CommandInputEndedTests {
+    private var validRound: Round? = null
+
+    @BeforeEach
+    fun setUp() {
+        validRound = Round(UUID.randomUUID(), 3, UUID.randomUUID(), RoundStatus.COMMAND_INPUT_ENDED)
+    }
+
+
+    @Test
+    fun newCommandInputEndedShouldInitialize() {
+        val commandInputEnded = CommandInputEnded(validRound!!)
+
+        assertThat(commandInputEnded)
+            .matches(validRound!!)
+    }
+
+    @Test
+    fun newCommandInputEndedShouldThrowWhenRoundStatusInvalid() {
+        val invalidRound = Round(UUID.randomUUID(), 3, UUID.randomUUID(), RoundStatus.COMMAND_INPUT_STARTED)
+
+        assertThatThrownBy {
+            CommandInputEnded(invalidRound)
+        }
+    }
+}
--- src/test/kotlin/microservice/dungeon/game/unittests/model/round/domain/RoundStartedTests.kt
+++ src/test/kotlin/microservice/dungeon/game/unittests/model/round/domain/RoundStartedTests.kt
@@ -1,28 +1,44 @@
-//package microservice.dungeon.game.unittests.model.round.domain
-//
-//import microservice.dungeon.game.aggregates.round.domain.Round
-//import microservice.dungeon.game.aggregates.round.domain.RoundStatus
-//import microservice.dungeon.game.aggregates.round.events.RoundStarted
-//import org.junit.jupiter.api.Assertions.*
-//import org.junit.jupiter.api.Test
-//import java.lang.Exception
-//import java.util.*
-//
-//class RoundStartedTests {
-//
-//    @Test
-//    fun checkDefaultValuesTest() {
-//        val round = Round(UUID.randomUUID(), 3, UUID.randomUUID(), RoundStatus.COMMAND_INPUT_STARTED)
-//        val roundStarted = RoundStarted(round)
-//        assertEquals(roundStarted.getTopic(), "testTopic")
-//        assertEquals(roundStarted.getEventName(), "roundStarted")
-//    }
-//
-//    @Test
-//    fun preventRoundStartedCreationWithWrongRoundStatusTest() {
-//        val round = Round(UUID.randomUUID(), 3, UUID.randomUUID(), RoundStatus.COMMAND_INPUT_ENDED)
-//        assertThrows(Exception::class.java) {
-//            val roundStarted = RoundStarted(round)
-//        }
-//    }
-//}
+package microservice.dungeon.game.unittests.model.round.domain
+
+import microservice.dungeon.game.aggregates.round.domain.Round
+import microservice.dungeon.game.aggregates.round.domain.RoundStatus
+import microservice.dungeon.game.aggregates.round.events.RoundStarted
+import microservice.dungeon.game.assertions.CustomAssertions.Companion.assertThat
+import org.assertj.core.api.Assertions.assertThatThrownBy
+import org.junit.jupiter.api.Assertions.*
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+import org.springframework.data.repository.query.Param
+import java.lang.Exception
+import java.util.*
+import java.util.stream.Stream
+
+class RoundStartedTests {
+    private var validRound: Round? = null
+
+    @BeforeEach
+    fun setUp() {
+        validRound = Round(UUID.randomUUID(), 3, UUID.randomUUID(), RoundStatus.COMMAND_INPUT_STARTED)
+    }
+
+
+    @Test
+    fun newRoundStartedShouldInitialize() {
+        val roundStarted = RoundStarted(validRound!!)
+
+        assertThat(roundStarted)
+            .matches(validRound!!)
+    }
+
+    @Test
+    fun newRoundStartedShouldThrowWhenInvalidRoundStatus() {
+        val invalidRound = Round(UUID.randomUUID(), 3, UUID.randomUUID(), RoundStatus.COMMAND_INPUT_ENDED)
+
+        assertThatThrownBy {
+            RoundStarted(invalidRound)
+        }
+    }
+}
