29ecb33009f520766c7afb891348be85a29dccd1
dadepu <danielkoellgen@gmail.com>
2021-11-21 16:29:42 +0100

    feat(player): implement player

---
+++ src/main/kotlin/microservice/dungeon/game/aggregates/player/domain/Player.kt
@@ -0,0 +1,39 @@
+package microservice.dungeon.game.aggregates.player.domain
+
+import org.hibernate.annotations.Type
+import java.util.*
+import javax.persistence.Entity
+import javax.persistence.Id
+import javax.persistence.Index
+import javax.persistence.Table
+
+@Entity
+@Table(name = "players", indexes = [
+    Index(name = "playerUniqueIndexFromPlayerToken", columnList = "playerToken", unique = true),
+    Index(name = "playerUniqueIndexFromUsername", columnList = "userName", unique = true),
+    Index(name = "playerUniqueIndexFromMailAddress", columnList = "mailAddress", unique = true)
+])
+class Player constructor(
+    private var userName: String,
+    private var mailAddress: String,
+    private var firstName: String,
+    private var lastName: String,
+) {
+    @Id
+    @Type(type="uuid-char")
+    private var playerId: UUID = UUID.randomUUID()
+    @Type(type="uuid-char")
+    private var playerToken: UUID = UUID.randomUUID()
+
+    fun getPlayerId(): UUID = playerId
+
+    fun getPlayerToken(): UUID = playerToken
+
+    fun getUserName(): String = userName
+
+    fun getMailAddress(): String = mailAddress
+
+    fun getFirstName(): String = firstName
+
+    fun getLastName(): String = lastName
+}
