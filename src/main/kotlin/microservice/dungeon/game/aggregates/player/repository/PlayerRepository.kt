9d527a7ccec71cd5eef946705a3c05bfd692aef7
dadepu <danielkoellgen@gmail.com>
2021-11-22 11:05:59 +0100

    feat(player): make playerRepository save and find player by id

---
+++ src/main/kotlin/microservice/dungeon/game/aggregates/player/repository/PlayerRepository.kt
@@ -0,0 +1,9 @@
+package microservice.dungeon.game.aggregates.player.repository
+
+import microservice.dungeon.game.aggregates.player.domain.Player
+import org.springframework.data.repository.CrudRepository
+import java.util.*
+
+interface PlayerRepository: CrudRepository<Player, UUID> {
+
+}
