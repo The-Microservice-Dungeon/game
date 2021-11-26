2422c2301294dc7bb4f711b12d68543694c2691c
Dennis Ceh <ceh.dennis@gmail.com>
2021-11-26 16:40:32 +0100

    game(insert player into game)

--- src/main/kotlin/microservice/dungeon/game/aggregates/game/repositories/GameRepository.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/game/repositories/GameRepository.kt
@@ -1,11 +1,14 @@
 package microservice.dungeon.game.aggregates.game.repositories
 
 import microservice.dungeon.game.aggregates.game.domain.Game
+import microservice.dungeon.game.aggregates.game.domain.PlayersInGame
 import org.springframework.data.repository.CrudRepository
 import java.util.*
 
 interface GameRepository : CrudRepository<Game, UUID> {
 
     fun findByGameId(gameId: UUID): Optional<Game>
+    abstract fun save(player: PlayersInGame)
+
 
 }
