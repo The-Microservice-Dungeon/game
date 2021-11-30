3e62a55e0e4c9389451ebf10ec8c6b9f0465d457
BrokenButler <castellobutler@gmail.com>
2021-11-30 18:39:04 +0100

    Moved The CommandRepository and CommandService to the new file structure
    Signed-off-by: BrokenButler <castellobutler@gmail.com>
    
    Took 39 seconds

---
+++ src/main/kotlin/microservice/dungeon/game/commands/services/CommandService.kt
@@ -0,0 +1,28 @@
+package microservice.dungeon.game.commands.services
+
+import microservice.dungeon.game.commands.domain.Command
+import microservice.dungeon.game.commands.dtos.CommandDTO
+import microservice.dungeon.game.commands.repository.CommandRepository
+import org.springframework.stereotype.Service
+import java.util.*
+
+@Service
+class CommandService(private val commandRepository: CommandRepository) {
+
+    fun getAllCommands(): List<Command> =
+        commandRepository.findAll() //TODO possibly have to add a second repo to save the list of commands and their respective round number
+
+    fun save(dto: CommandDTO): UUID {
+        val command: Command = Command.fromDTO(dto)
+        commandRepository.save(command)
+        return command.transactionId
+    }
+
+    fun getCommandById(commandId: UUID): Optional<Command> = commandRepository.findById(commandId)
+
+    fun deleteCommandById(commandId: UUID): Optional<Unit> {
+        return commandRepository.findById(commandId).map { command ->
+            commandRepository.delete(command)
+        }
+    }
+}
