db1720f657704225e940821b1e7c3ffb3481fc8f
BrokenButler <castellobutler@gmail.com>
2021-12-02 10:17:14 +0100

    Reworked the command saving procedure
    Signed-off-by: BrokenButler <castellobutler@gmail.com>
    
    Took 44 minutes

--- src/main/kotlin/microservice/dungeon/game/aggregates/commands/repositories/CommandRepository.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/commands/repositories/CommandRepository.kt
@@ -1,9 +1,13 @@
 package microservice.dungeon.game.aggregates.commands.repositories
 
 import microservice.dungeon.game.aggregates.commands.domain.Command
+import microservice.dungeon.game.aggregates.commands.domain.CommandType
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 import java.util.*
 
 @Repository
-interface CommandRepository : JpaRepository<Command, UUID>
+interface CommandRepository : JpaRepository<Command, UUID> {
+    fun findByRobotIdIn(robotId: UUID): List<Command>
+    fun findByCommandTypeIn(commandType: CommandType): List<Command>
+}
