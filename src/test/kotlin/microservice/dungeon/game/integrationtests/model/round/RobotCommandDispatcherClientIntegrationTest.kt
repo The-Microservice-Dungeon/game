ca5eaa7fd99be0183dd207553dfe41a390a2d89b
dadepu <danielkoellgen@gmail.com>
2021-12-06 13:18:46 +0100

    test(round): robotCommandDispatcherClient should allow to send blocking commands to robot

---
+++ src/test/kotlin/microservice/dungeon/game/integrationtests/model/round/RobotCommandDispatcherClientIntegrationTest.kt
@@ -0,0 +1,66 @@
+package microservice.dungeon.game.integrationtests.model.round
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import microservice.dungeon.game.aggregates.command.dtos.BlockCommandDTO
+import microservice.dungeon.game.aggregates.command.dtos.RobotCommandWrapperDTO
+import microservice.dungeon.game.aggregates.round.web.RobotCommandDispatcherClient
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+import java.util.*
+
+class RobotCommandDispatcherClientIntegrationTest {
+    private var mockWebServer: MockWebServer? = null
+    private var robotCommandDispatcherClient: RobotCommandDispatcherClient? = null
+    private val objectMapper = ObjectMapper().findAndRegisterModules()
+
+
+    @BeforeEach
+    fun setUp() {
+        mockWebServer = MockWebServer()
+        mockWebServer!!.start()
+        robotCommandDispatcherClient = RobotCommandDispatcherClient(mockWebServer!!.url("/").toString())
+    }
+
+
+    @Test
+    fun shouldAllowToSendBlockingCommands() {
+        //given
+        val inputCommands = listOf(
+            BlockCommandDTO(UUID.randomUUID(), UUID.randomUUID()),
+            BlockCommandDTO(UUID.randomUUID(), UUID.randomUUID())
+        )
+        val mockResponse = MockResponse()
+            .setResponseCode(202)
+        mockWebServer!!.enqueue(mockResponse)
+
+        //when
+        robotCommandDispatcherClient!!.sendBlockingCommandsToRobot(inputCommands)
+
+        //and
+        val recordedRequest = mockWebServer!!.takeRequest()
+        val recordedRobotCommandWrapperDTO = objectMapper.readValue(
+            recordedRequest.body.readUtf8(),
+            RobotCommandWrapperDTO::class.java
+        )
+        val recordedBlockingCommandDTOs: List<BlockCommandDTO> = recordedRobotCommandWrapperDTO.commands.map {
+                x -> BlockCommandDTO.fromString(x)
+        }
+
+        //then
+        assertThat(recordedRequest.method)
+            .isEqualTo("POST")
+        assertThat(recordedRequest.path)
+            .isEqualTo("/commands")
+        assertThat(recordedRequest.getHeader(HttpHeaders.CONTENT_TYPE))
+            .isEqualTo(MediaType.APPLICATION_JSON.toString())
+
+        //and
+        assertThat(recordedBlockingCommandDTOs)
+            .isEqualTo(inputCommands)
+    }
+}
