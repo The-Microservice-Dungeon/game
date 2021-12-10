6db6577de904b300f9517683243e20bd0b3ee123
dadepu <danielkoellgen@gmail.com>
2021-12-10 22:04:01 +0100

    fix(round): annotate tradingCommandDispatcherClient as Component

--- src/main/kotlin/microservice/dungeon/game/aggregates/round/web/TradingCommandDispatcherClient.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/round/web/TradingCommandDispatcherClient.kt
@@ -9,8 +9,10 @@ import org.springframework.beans.factory.annotation.Value
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
+@Component
 class TradingCommandDispatcherClient @Autowired constructor(
     @Value(value = "\${rest.trading.baseurl}")
     private val tradingBaseURL: String
