39f141706e6ae41bc9398881c8eb48ee736780d3
dadepu <danielkoellgen@gmail.com>
2021-12-18 10:15:00 +0100

    feat(kafka): make kafkaRobotConsumer destroy robot when receiving message

--- src/main/kotlin/microservice/dungeon/game/messaging/consumer/robot/KafkaRobotConsumer.kt
+++ src/main/kotlin/microservice/dungeon/game/messaging/consumer/robot/KafkaRobotConsumer.kt
@@ -1,38 +1,24 @@
 package microservice.dungeon.game.messaging.consumer.robot
 
-import com.fasterxml.jackson.databind.JsonNode
 import com.fasterxml.jackson.databind.ObjectMapper
-import microservice.dungeon.game.aggregates.eventconsumer.services.EventConsumerService
 import microservice.dungeon.game.aggregates.robot.services.RobotService
-import microservice.dungeon.game.messaging.consumer.robot.events.AbstractRobotEvent
-import microservice.dungeon.game.messaging.consumer.robot.events.RobotCreated
-import microservice.dungeon.game.messaging.consumer.robot.events.RobotDestroyed
+import microservice.dungeon.game.messaging.consumer.robot.dtos.RobotDestroyedDto
+import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.stereotype.Component
-import java.util.*
 
-//@Component
+@Component
 class KafkaRobotConsumer @Autowired constructor(
-    private val eventConsumerService: EventConsumerService,
     private val robotService: RobotService
 ) {
-    @KafkaListener(topics = ["\${kafka.topicSubRobot}"])
-    fun consume(message: String) {
-        // read message
-        //TODO
-    }
-
-    fun buildEvent(type: RobotEventType, message: String): AbstractRobotEvent {
-        return when (type) {
-            RobotEventType.ROBOT_CREATED -> RobotCreated(robotService, message)
-            RobotEventType.ROBOT_DESTROYED -> RobotDestroyed(robotService, message)
+    @KafkaListener(id = "\${robot-destroyed-listener}", topics = ["\${kafka.topicSubRobotDestroyed}"])
+    fun destroyRobot(record: ProducerRecord<String, String>) {
+        try {
+            val payload = RobotDestroyedDto.makeFromSerialization(record.value())
+            robotService.destroyRobot(payload.robotId)
+        } catch (e: Exception){
+            //TODO("log any errors")
         }
-    }
-
-    enum class RobotEventType {
-        ROBOT_CREATED,
-        ROBOT_DESTROYED
     }
 }
