0563de3fb9c10a35ef80d2f6411134fd37c26613
dadepu <danielkoellgen@gmail.com>
2021-12-18 10:47:47 +0100

    feat(kafka): make kafkaRobotConsumer create new robot when receiving robot spawned message

--- src/main/kotlin/microservice/dungeon/game/messaging/consumer/robot/KafkaRobotConsumer.kt
+++ src/main/kotlin/microservice/dungeon/game/messaging/consumer/robot/KafkaRobotConsumer.kt
@@ -2,6 +2,7 @@ package microservice.dungeon.game.messaging.consumer.robot
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import microservice.dungeon.game.aggregates.robot.services.RobotService
+import microservice.dungeon.game.messaging.consumer.robot.dtos.RobotCreatedDto
 import microservice.dungeon.game.messaging.consumer.robot.dtos.RobotDestroyedDto
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,7 +13,18 @@ import org.springframework.stereotype.Component
 class KafkaRobotConsumer @Autowired constructor(
     private val robotService: RobotService
 ) {
-    @KafkaListener(id = "\${robot-destroyed-listener}", topics = ["\${kafka.topicSubRobotDestroyed}"])
+
+    @KafkaListener(id = "\${kafka.topicSubRobotSpawned.group}", topics = ["\${kafka.topicSubRobotSpawned}"])
+    fun makeNewRobot(record: ProducerRecord<String, String>) {
+        try {
+            val payload = RobotCreatedDto.makeFromSerialization(record.value())
+            robotService.newRobot(payload.robotId, payload.playerId)
+        } catch (e: Exception) {
+            //TODO("log any errors")
+        }
+    }
+
+    @KafkaListener(id = "\${kafka.topicSubRobotDestroyed.group}", topics = ["\${kafka.topicSubRobotDestroyed}"])
     fun destroyRobot(record: ProducerRecord<String, String>) {
         try {
             val payload = RobotDestroyedDto.makeFromSerialization(record.value())
