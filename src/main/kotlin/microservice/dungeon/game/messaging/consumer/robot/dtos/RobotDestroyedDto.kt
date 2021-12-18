f74af512c806c2a2bc09b2416298bc6251bc02f3
dadepu <danielkoellgen@gmail.com>
2021-12-18 09:55:52 +0100

    feat(kafka): implement RobotDestroyedDto

---
+++ src/main/kotlin/microservice/dungeon/game/messaging/consumer/robot/dtos/RobotDestroyedDto.kt
@@ -0,0 +1,22 @@
+package microservice.dungeon.game.messaging.consumer.robot.dtos
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import java.util.*
+
+class RobotDestroyedDto(
+    val robotId: UUID,
+    val playerId: UUID
+) {
+    companion object {
+        private val objectMapper = ObjectMapper().findAndRegisterModules()
+
+        fun makeFromSerialization(serial: String): RobotDestroyedDto =
+            objectMapper.readValue(serial, RobotDestroyedDto::class.java)
+    }
+
+
+    override fun equals(other: Any?): Boolean =
+        (other is RobotDestroyedDto)
+                && robotId == other.robotId
+                && playerId == other.playerId
+}
