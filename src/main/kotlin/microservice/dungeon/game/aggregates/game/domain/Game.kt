0f2e4207888d6f513f00628bf0f0ca48dcd36c24
Dennis Ceh <ceh.dennis@gmail.com>
2022-01-04 11:35:51 +0100

    fix get/games error

--- src/main/kotlin/microservice/dungeon/game/aggregates/game/domain/Game.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/game/domain/Game.kt
@@ -3,6 +3,7 @@ package microservice.dungeon.game.aggregates.game.domain
 import microservice.dungeon.game.aggregates.core.MethodNotAllowedForStatusException
 import microservice.dungeon.game.aggregates.player.domain.Player
 import org.hibernate.annotations.Type
+
 import java.time.LocalDateTime
 import java.time.LocalTime
 import java.util.*
@@ -14,15 +15,15 @@ import javax.validation.constraints.NotBlank
 @Table(name = "games", indexes = [
     Index(name = "GameIndex", columnList = "gameId", unique = true)
 ])
-class Game(
+class Game constructor(
     @Id
     @Type(type="uuid-char")
     private val gameId: UUID = UUID.randomUUID(),
     private var gameStatus: GameStatus = GameStatus.CREATED,
-    @get: NotBlank
+
     private var maxPlayers: Int = 0,
-    @get: NotBlank
-    private var maxRounds: Int = 0,
+
+    private var maxRounds: Int = 1,
 
     private var roundDuration: Long = 60000, // in ms
     private var commandCollectDuration: Double = 45000.00, // in ms
