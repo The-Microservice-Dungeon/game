e97d178fbc00a5f0e06170712dabd87e73bd9424
dadepu <danielkoellgen@gmail.com>
2022-01-06 13:03:50 +0100

    fix(game): conform PlayerJoinedEventDto to EventAPI

--- src/main/kotlin/microservice/dungeon/game/aggregates/game/dtos/PlayerJoinedEventDto.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/game/dtos/PlayerJoinedEventDto.kt
@@ -3,21 +3,17 @@ package microservice.dungeon.game.aggregates.game.dtos
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import microservice.dungeon.game.aggregates.core.EventDto
-import microservice.dungeon.game.aggregates.domainprimitives.EventTime
-import microservice.dungeon.game.aggregates.game.domain.GameStatus
-import java.lang.RuntimeException
 import java.util.*
 
+
 class PlayerJoinedEventDto(
-    val playerId: UUID,
-    val lobbyAction: String
+    val userId: UUID,
+    val lobbyAction: String = "joined"
 ): EventDto {
-    constructor(playerId: UUID ):
-            this(playerId,"joined")
+    constructor(playerId: UUID ): this(playerId,"joined")
 
     override fun serialize(): String {
         val objectMapper = ObjectMapper().findAndRegisterModules()
         return objectMapper.writeValueAsString(this)
     }
-
 }
