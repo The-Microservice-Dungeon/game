afbb465a68d1d5408d65065c83ed4d4420334128
dadepu <danielkoellgen@gmail.com>
2022-01-10 15:35:34 +0100

    fix(game): annotate MapGameWorldsClient w/ component as singleton

--- src/main/kotlin/microservice/dungeon/game/aggregates/game/web/MapGameWorldsClient.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/game/web/MapGameWorldsClient.kt
@@ -5,11 +5,15 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import microservice.dungeon.game.aggregates.game.web.dto.NewGameWorldDto
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
+import org.springframework.context.annotation.Scope
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
 import org.springframework.http.MediaType
+import org.springframework.stereotype.Component
 import org.springframework.web.reactive.function.client.WebClient
 
+@Component
+@Scope("singleton")
 class MapGameWorldsClient @Autowired constructor(
     @Value(value = "\${rest.map.baseurl}")
     private val mapBaseUrl: String
