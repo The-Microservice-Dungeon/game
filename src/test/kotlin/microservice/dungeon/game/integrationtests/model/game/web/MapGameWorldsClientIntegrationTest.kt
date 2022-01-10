ec612c039899d07945109c1a3989be618e05216d
dadepu <danielkoellgen@gmail.com>
2022-01-10 15:11:35 +0100

    test(game): MapGameWorldsClient should allow to create a new game-world

---
+++ src/test/kotlin/microservice/dungeon/game/integrationtests/model/game/web/MapGameWorldsClientIntegrationTest.kt
@@ -0,0 +1,62 @@
+package microservice.dungeon.game.integrationtests.model.game.web
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.fasterxml.jackson.module.kotlin.KotlinModule
+import com.fasterxml.jackson.module.kotlin.readValue
+import microservice.dungeon.game.aggregates.game.web.MapGameWorldsClient
+import microservice.dungeon.game.aggregates.game.web.dto.NewGameWorldDto
+import okhttp3.mockwebserver.MockResponse
+import okhttp3.mockwebserver.MockWebServer
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.BeforeEach
+import org.junit.jupiter.api.Test
+import org.springframework.http.HttpHeaders
+import org.springframework.http.MediaType
+
+class MapGameWorldsClientIntegrationTest {
+    private var mockWebServer: MockWebServer? = null
+    private var mapGameWorldsClient: MapGameWorldsClient? = null
+
+    private val objectMapper = ObjectMapper().registerModule(KotlinModule())
+
+
+    @BeforeEach
+    fun setUp() {
+        mockWebServer = MockWebServer()
+        mockWebServer!!.start()
+        mapGameWorldsClient = MapGameWorldsClient(mockWebServer!!.url("/").toString())
+    }
+
+    @Test
+    fun shouldAllowToCreateANewGameWorld() {
+        // given
+        val mockResponse = MockResponse()
+            .setResponseCode(201)
+        mockWebServer!!.enqueue(mockResponse)
+
+        // when
+        val response = mapGameWorldsClient!!.createNewGameWorld()
+
+        // and
+        val capturedRequest = mockWebServer!!.takeRequest()
+        val capturedNewGameWorldDto: NewGameWorldDto = objectMapper.readValue(
+            capturedRequest.body.readUtf8()
+        )
+
+        // then
+        assertThat(response)
+            .isTrue
+
+        // and
+        assertThat(capturedRequest.method)
+            .isEqualTo("POST")
+        assertThat(capturedRequest.path)
+            .isEqualTo("/gameworlds")
+        assertThat(capturedRequest.getHeader(HttpHeaders.CONTENT_TYPE))
+            .isEqualTo(MediaType.APPLICATION_JSON.toString())
+
+        // and
+        assertThat(capturedNewGameWorldDto)
+            .isEqualTo(capturedNewGameWorldDto)
+    }
+}
