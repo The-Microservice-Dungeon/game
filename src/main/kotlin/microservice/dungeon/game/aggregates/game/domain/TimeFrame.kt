d3b493ad94c226d3cda4ee6d5c57c43841942142
dadepu <danielkoellgen@gmail.com>
2022-01-14 22:04:18 +0100

    feat(game): add TimeFrame

---
+++ src/main/kotlin/microservice/dungeon/game/aggregates/game/domain/TimeFrame.kt
@@ -0,0 +1,23 @@
+package microservice.dungeon.game.aggregates.game.domain
+
+import java.time.LocalDateTime
+import java.time.temporal.ChronoUnit
+
+class TimeFrame (
+   private val totalRoundDurationInMS: Long,
+   private val commandInputLengthInPercent: Int,
+   private val timeStarted: LocalDateTime
+) {
+    constructor(totalRoundDurationInMS: Long, commandInputLengthInPercent: Int):
+            this(totalRoundDurationInMS, commandInputLengthInPercent, LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS))
+
+    fun getCommandInputTimeFrameInMS(): Long {
+        return (totalRoundDurationInMS * commandInputLengthInPercent) / 100
+    }
+
+    fun getExecutionTimeFrameInMS(): Long {
+        val timePassedInMS = ChronoUnit.MILLIS.between(timeStarted, LocalDateTime.now().truncatedTo(ChronoUnit.MILLIS))
+        val remainingTimeInMS = totalRoundDurationInMS - timePassedInMS
+        return if (remainingTimeInMS > 0) remainingTimeInMS else 0
+    }
+}
