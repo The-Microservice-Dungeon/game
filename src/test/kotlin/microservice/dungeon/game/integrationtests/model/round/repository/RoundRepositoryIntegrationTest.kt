8e87f827ff9439a20b6a40ed31949ace5cddaddf
dadepu <danielkoellgen@gmail.com>
2022-01-16 12:09:59 +0100

    test(round): [IntegrationTest] RoundRepository should allow to fetch round by roundId

--- src/test/kotlin/microservice/dungeon/game/integrationtests/model/round/repository/RoundRepositoryIntegrationTest.kt
+++ src/test/kotlin/microservice/dungeon/game/integrationtests/model/round/repository/RoundRepositoryIntegrationTest.kt
@@ -6,17 +6,13 @@ import microservice.dungeon.game.aggregates.round.domain.Round
 import microservice.dungeon.game.aggregates.round.domain.RoundStatus
 import microservice.dungeon.game.aggregates.round.repositories.RoundRepository
 import org.assertj.core.api.Assertions.assertThat
-import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertTrue
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.kafka.test.context.EmbeddedKafka
-import org.springframework.test.annotation.DirtiesContext
-import org.springframework.transaction.annotation.Transactional
 import org.springframework.transaction.support.TransactionTemplate
-import java.util.*
 
 @SpringBootTest(properties = [
     "kafka.bootstrapAddress=localhost:29098"
@@ -24,53 +20,27 @@ import java.util.*
 @EmbeddedKafka(partitions = 1, brokerProperties = ["listeners=PLAINTEXT://localhost:29098", "port=29098"])
 class RoundRepositoryIntegrationTest @Autowired constructor(
     private val gameRepository: GameRepository,
-    private val roundRepository: RoundRepository,
-    private val transactionTemplate: TransactionTemplate
+    private val roundRepository: RoundRepository
 ) {
+    private val game = Game(10, 100)
+
     @BeforeEach
     fun initialize() {
         roundRepository.deleteAll()
     }
 
     @Test
-    fun saveRoundShouldPersistRound() {
-        val game = Game(10, 100)
+    fun shouldAllowToSaveAndFetchRounds() {
+        // given
+        val round = Round(game = game, roundNumber = 3, roundStatus = RoundStatus.COMMAND_INPUT_STARTED)
         gameRepository.save(game)
-        val roundNumber = 3
-        val round = Round(game = game, roundNumber = roundNumber, roundStatus = RoundStatus.COMMAND_INPUT_STARTED)
+        roundRepository.save(round)
 
-        val roundId = transactionTemplate.execute {
-            roundRepository.save(round)
-            round.getRoundId()
-        }!!
-        val loadedRound = transactionTemplate.execute {
-            roundRepository.findById(roundId).get()
-        }!!
+        // when
+        val capturedRound: Round = roundRepository.findById(round.getRoundId()).get()
 
-        assertThat(loadedRound.getRoundId())
-            .isEqualTo(roundId)
-        assertThat(loadedRound.getRoundStarted())
-            .isEqualTo(round.getRoundStarted())
+        // then
+        assertThat(capturedRound.isEqualByVal(round))
+            .isTrue
     }
-
-    @Test
-    fun findByGameIdAndRoundNumberShouldFindRound() {
-        assertTrue(false)
-    }
-//        val game = Game(10, 100)
-//        val gameId = game.getGameId()
-//        val roundNumber = 3
-//        val round = Round(game = game, roundNumber = roundNumber, roundStatus = RoundStatus.COMMAND_INPUT_STARTED)
-//
-//        val roundId = transactionTemplate.execute {
-//            roundRepository.save(round)
-//            round.getRoundId()
-//        }!!
-//        val loadedRound = transactionTemplate.execute {
-//            roundRepository.findByGameIdAndRoundNumber(gameId, roundNumber).get()
-//        }!!
-//
-//        assertThat(loadedRound.getRoundId())
-//            .isEqualTo(roundId)
-//    }
 }
