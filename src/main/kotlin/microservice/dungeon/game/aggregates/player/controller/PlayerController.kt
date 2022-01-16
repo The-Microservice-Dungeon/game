b35eaa13b04eb3d167d5c5c8548545a53135c15f
dadepu <danielkoellgen@gmail.com>
2022-01-16 13:51:27 +0100

    feat(player): make PlayerController allow to Players

--- src/main/kotlin/microservice/dungeon/game/aggregates/player/controller/PlayerController.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/player/controller/PlayerController.kt
@@ -1,19 +1,20 @@
 package microservice.dungeon.game.aggregates.player.controller
 
 import microservice.dungeon.game.aggregates.player.controller.dtos.PlayerResponseDto
+import microservice.dungeon.game.aggregates.player.domain.Player
 import microservice.dungeon.game.aggregates.player.domain.PlayerAlreadyExistsException
+import microservice.dungeon.game.aggregates.player.repository.PlayerRepository
 import microservice.dungeon.game.aggregates.player.services.PlayerService
 import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RestController
 class PlayerController @Autowired constructor(
-    private val playerService: PlayerService
+    private val playerService: PlayerService,
+    private val playerRepository: PlayerRepository
 ){
     companion object {
         private val logger = KotlinLogging.logger {}
@@ -36,6 +37,26 @@ class PlayerController @Autowired constructor(
             logger.warn(e.message)
             logger.trace("Responding with 403.")
             ResponseEntity(HttpStatus.FORBIDDEN)
+        }
+    }
+
+    @GetMapping("/players", produces = ["application/json"])
+    fun getPlayer(@RequestParam(name = "name") userName: String, @RequestParam(name = "mail") userMail: String): ResponseEntity<PlayerResponseDto> {
+        logger.debug("Request to fetch player-details received ... [playerName=${userName}]")
+
+        return try {
+            val player: Player = playerRepository.findByUserNameAndMailAddress(userName, userMail).get()
+            val responsePlayer = PlayerResponseDto.makeFromPlayer(player)
+
+            logger.debug("Request successful. Player found. [playerName=${player.getUserName()}]")
+            logger.trace("Responding with 200.")
+            ResponseEntity(responsePlayer, HttpStatus.OK)
+
+        } catch (e: Exception) {
+            logger.warn("Request to fetch player failed.")
+            logger.warn(e.message)
+            logger.trace("Responding with 404.")
+            ResponseEntity(HttpStatus.NOT_FOUND)
         }
     }
 }
