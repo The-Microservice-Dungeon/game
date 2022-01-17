1264f24a2da3c814061f1c8dd44dbf9c75cc937d
dadepu <danielkoellgen@gmail.com>
2022-01-17 12:49:57 +0100

    feat(command): make CommandController allow to accept new Commands.

--- src/main/kotlin/microservice/dungeon/game/aggregates/command/services/CommandService.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/command/services/CommandService.kt
@@ -1,12 +1,21 @@
 package microservice.dungeon.game.aggregates.command.services
 
+import microservice.dungeon.game.aggregates.command.controller.dto.CommandRequestDto
 import microservice.dungeon.game.aggregates.command.domain.Command
-import microservice.dungeon.game.aggregates.command.dtos.CommandDTO
+import microservice.dungeon.game.aggregates.command.domain.CommandArgumentException
+import microservice.dungeon.game.aggregates.command.domain.CommandPayload
+import microservice.dungeon.game.aggregates.command.domain.CommandType
 import microservice.dungeon.game.aggregates.command.repositories.CommandRepository
+import microservice.dungeon.game.aggregates.game.domain.Game
+import microservice.dungeon.game.aggregates.game.domain.GameNotFoundException
+import microservice.dungeon.game.aggregates.game.domain.GameStateException
 import microservice.dungeon.game.aggregates.game.repositories.GameRepository
+import microservice.dungeon.game.aggregates.player.domain.Player
+import microservice.dungeon.game.aggregates.player.domain.PlayerNotFoundException
 import microservice.dungeon.game.aggregates.player.repository.PlayerRepository
-import microservice.dungeon.game.aggregates.robot.domain.RobotStatus
 import microservice.dungeon.game.aggregates.robot.repositories.RobotRepository
+import microservice.dungeon.game.aggregates.round.domain.Round
+import mu.KotlinLogging
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import java.util.*
@@ -18,8 +27,65 @@ class CommandService @Autowired constructor(
     private val gameRepository: GameRepository,
     private val playerRepository: PlayerRepository
 ) {
+    companion object {
+        private val logger = KotlinLogging.logger {}
+    }
 
-    fun save(dto: CommandDTO): UUID {
+    @Throws(PlayerNotFoundException::class, GameNotFoundException::class, GameStateException::class, CommandArgumentException::class)
+    fun createNewCommand(gameId: UUID, playerToken: UUID, robotId: UUID?, commandType: CommandType, commandRequestDTO: CommandRequestDto): UUID {
+        val transactionId: UUID = UUID.randomUUID()
+        val player: Player
+        val game: Game
+        val round: Round
+
+        try {
+            player = playerRepository.findByPlayerToken(playerToken).get()
+        } catch (e: Exception) {
+            logger.warn("TODO")
+            throw PlayerNotFoundException("TODO")
+        }
+        try {
+            game = gameRepository.findById(gameId).get()
+        } catch (e: Exception) {
+            logger.warn("TODO")
+            throw GameNotFoundException("TODO")
+        }
+        try {
+            round = game.getCurrentRound()!!
+        } catch (e: Exception) {
+            logger.warn("TODO")
+            throw GameStateException("TODO")
+        }
+        val robot = try {
+            robotRepository.findById(robotId!!).get()
+        } catch (e: Exception) {
+            null
+        }
+
+        // TODO COMMAND
+            // TODO PREVENT DUPLICATES
+            // TODO ONLY VALID COMMANDS
+        // TODO SAVE
+        val command: Command = Command(
+            commandId = transactionId,
+            round = round,
+            player = player,
+            robot = robot,
+            commandType = commandType,
+            commandPayload = CommandPayload(
+                planetId = commandRequestDTO.commandObject.planetId,
+                targetId = commandRequestDTO.commandObject.targetId,
+                itemName = commandRequestDTO.commandObject.itemName,
+                itemQuantity = commandRequestDTO.commandObject.itemQuantity
+            )
+        )
+        commandRepository.save(command)
+
+        logger.info("TODO")
+        return transactionId
+    }
+
+    fun save(dto: CommandRequestDto): UUID {
 //        val currentRoundNumber = 0 //TODO("REPLACE") gameRepository.findById(dto.gameId).get().getCurrentRoundCount()
 //
 //        val player = playerRepository.findByPlayerToken(dto.playerToken)
