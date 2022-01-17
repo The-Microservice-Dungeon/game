11615f4dba1ffc310ec3f227fc7954cf971ff0eb
dadepu <danielkoellgen@gmail.com>
2022-01-17 07:47:43 +0100

    refactor(messaging): add logging to KafkaProducer

--- src/main/kotlin/microservice/dungeon/game/messaging/producer/KafkaProducer.kt
+++ src/main/kotlin/microservice/dungeon/game/messaging/producer/KafkaProducer.kt
@@ -1,6 +1,7 @@
 package microservice.dungeon.game.messaging.producer
 
 import microservice.dungeon.game.aggregates.core.Event
+import mu.KotlinLogging
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.apache.kafka.common.header.internals.RecordHeader
 import org.springframework.beans.factory.annotation.Autowired
@@ -12,6 +13,10 @@ import java.time.format.DateTimeFormatter
 class KafkaProducer @Autowired constructor (
     private val kafkaTemplate: KafkaTemplate<String, String>
 ) : KafkaProducing {
+
+    companion object {
+        private val logger = KotlinLogging.logger {}
+    }
 
     override fun send(event: Event) {
         val record = ProducerRecord<String, String>(event.getTopic(), event.toDTO().serialize())
@@ -30,10 +35,19 @@ class KafkaProducer @Autowired constructor (
         record.headers().add(
             RecordHeader("type", event.getEventName().toByteArray())
         )
+
+        logger.debug("Publishing ${event.getEventName()} to ${event.getTopic()}.")
+        logger.trace(eventToString(event))
         kafkaTemplate.send(record)
     }
 
     override fun send(record: ProducerRecord<String, String>) {
         kafkaTemplate.send(record)
+    }
+
+    private fun eventToString(event: Event): String {
+        return "Header(eventId:${event.getId()},transactionId:${event.getTransactionId()},version:${event.getVersion()}," +
+                "timestamp:${event.getOccurredAt().getFormattedTime()},type:${event.getEventName()})\r\n" +
+                "Payload(${event.toDTO().serialize()})"
     }
 }
