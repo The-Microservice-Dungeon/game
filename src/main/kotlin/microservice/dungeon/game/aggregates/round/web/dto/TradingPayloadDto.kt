25caf086f091c9c55f772d7b06b9b657d55c2391
dadepu <danielkoellgen@gmail.com>
2022-01-18 07:59:33 +0100

    refactor(command): move TradingPayloadDto from /command to /round

--- src/main/kotlin/microservice/dungeon/game/aggregates/round/web/dto/TradingPayloadDto.kt
+++ src/main/kotlin/microservice/dungeon/game/aggregates/round/web/dto/TradingPayloadDto.kt
@@ -1,15 +1,15 @@
-package microservice.dungeon.game.aggregates.command.dtos
+package microservice.dungeon.game.aggregates.round.web.dto
 
 import java.util.*
 
-class TradingPayload(
+class TradingPayloadDto (
     val commandType: String,
     val amount: Int,
     val planetId: UUID,
     val itemName: String
 ) {
     override fun equals(other: Any?): Boolean =
-        (other is TradingPayload)
+        (other is TradingPayloadDto)
                 && commandType == other.commandType
                 && amount == other.amount
                 && planetId == other.planetId
